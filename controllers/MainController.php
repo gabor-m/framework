83cff690cddfc377e7c7dccec286e905b04a4c96
Menyhért Gábor <gmenyhert92@gmail.com>
2019-03-10 18:35:06 +0100

    Hash alapú file storage

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -4,11 +4,16 @@ namespace app\controllers;
 use app\framework\Controller;
 use app\framework\Response;
 use app\framework\Mail;
+use app\framework\Storage;
 
 class MainController extends Controller {
 
     public function index($req) {
-        return Response::view("test");
+        // return Response::view("test");
+        
+        return [
+            "test" => Storage::addString(""),
+        ];
     }
     
     public function paramTest($req) {
