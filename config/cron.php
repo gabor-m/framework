c2a8bdf220055f460a4467dfda2ce91f8fa758b1
Menyhért Gábor <gmenyhert92@gmail.com>
2019-03-17 15:14:32 +0100

    Action, amivel le lehet kérni az utolsó cron frissítés dátumát

--- config/cron.php
+++ config/cron.php
@@ -3,6 +3,6 @@ use app\framework\cron\Cron;
 
 $job = Cron::add(function () {
     file_put_contents("storage/temp/" . time() . ".cron.tick", "HELLO WORLD");
-})->hourly("00, 01, 02, 03");
+})->hourly("05, 06");
 
 ?>
